2311b2f33572a369569fb0a740fee54441bbff98
Nez Mustafa <nezmustafa123@gmail.com>
2022-04-27 21:07:31 +0100

    get date until new year create set interbal function

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const countTo = "1 Jan 2023";
 //specify count to variable
 const c = setInterval(() => {
+  const endDate = new Date(countTo);
+  const currentDate = new Date();
+  const totalSeconds = (endDate = currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24; //what's left after getting number of days
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-};
+  const countDown = document.getElementById("countdown");
+}, 1000);
