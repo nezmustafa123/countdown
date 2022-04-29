5c92193ac1bc42606ed440b4238085dd8fe2cacd
Nez Mustafa <nezmustafa123@gmail.com>
2022-04-29 18:14:54 +0100

    change text content in countdown element

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const countTo = "1 Jan 2023";
 const c = setInterval(() => {
   const endDate = new Date(countTo);
   const currentDate = new Date();
-  const totalSeconds = (endDate = currentDate) / 1000;
-  const days = Math.floor(totalSeconds / 3600 / 24);
+  const totalSeconds = (endDate - currentDate) / 1000; //1000 milli seconds in one second
+
+  const days = Math.floor(totalSeconds / 3600 / 24); //3600 seconds in an hour
   const hours = Math.floor(totalSeconds / 3600) % 24; //what's left after getting number of days
-  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const minutes = Math.floor(totalSeconds / 60) % 60; //get minutes divide by 60
   const seconds = Math.floor(totalSeconds) % 60;
 
   const countDown = document.getElementById("countdown");
+
+  countDown.textContent = `${days}Days ${hours}Mrs : ${minutes} Min : ${}`;
 }, 1000);
